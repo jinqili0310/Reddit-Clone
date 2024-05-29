29f6bd664643440227398b842391ce8badb915b7
jinqili0310 <jinqi.li.310@gmail.com>
2024-05-29 16:32:43 -0700

    24:44

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
-import { Navbar } from "@/components/Navbar"
+import Navbar from "@/components/Navbar"
 
 export const metadata = {
   title: 'Reddit Clone',
