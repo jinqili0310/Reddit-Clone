9e1eb903c508cdee55a695e1f6ba9ea185b24348
jinqili0310 <jinqi.li.310@gmail.com>
2024-06-10 17:53:28 -0700

    41:04

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,30 @@
-const Login = () => {
-    
-}
+import Link from "next/link";
+import { Icons } from "./Icons";
+import UserAuthForm from "./UserAuthForm";
+
+const Login = ({}) => {
+  return (
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" style={{fill: 'currentColor'}} />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+        <p className="text-sm max-w-xs mx-auto">
+          By continuing, you are setting up a Reddit account and agree to our
+          User Agreement and Privacy Policy.
+        </p>
+      </div>
+      <UserAuthForm />
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        New to Reddit?{" "}
+        <Link
+          href="/signup"
+          className="hover:text-brand text-sm underline underline-offset-4"
+        >
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
+};
 
 export default Login;
