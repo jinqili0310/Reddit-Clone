8cda08df2dac3ad989ca1c32ccc32f126584cbf9
jinqili0310 <jinqi.li.310@gmail.com>
2024-06-11 17:44:17 -0700

    1:15:29

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,30 +1,36 @@
-import Link from "next/link";
+import { FC } from "react";
 import { Icons } from "./Icons";
 import UserAuthForm from "./UserAuthForm";
+import Link from "next/link";
 
-const Login = () => {
+const Signup: FC = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-6 w-6" style={{fill: 'currentColor'}} />
-        <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+        <Icons.logo
+          className="mx-auto h-6 w-6"
+          style={{ fill: "currentColor" }}
+        />
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Register Account
+        </h1>
         <p className="text-sm max-w-xs mx-auto">
-          By continuing, you are setting up a Reddit account and agree to our
+          By continuing, you are setting up a Breadit account and agree to our
           User Agreement and Privacy Policy.
         </p>
       </div>
       <UserAuthForm />
       <p className="px-8 text-center text-sm text-muted-foreground">
-        New to Reddit?{" "}
+        Already has an account?{" "}
         <Link
-          href="/signup"
+          href="/login"
           className="hover:text-brand text-sm underline underline-offset-4"
         >
-          Sign Up
+          Login
         </Link>
       </p>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,5 +1,48 @@
-const UserAuthForm = ({}) => {
-  return <div>UserAuthForm</div>
-}
+"use client";
 
-export default UserAuthForm
+import { cn } from "@/lib/utils";
+import { FC, useState } from "react";
+import { Button } from "./ui/Button";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { toast } from "@/hooks/use-toast";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+              className="w-full"
+              type="button"
+              disabled={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
